8a596e3ada085db5913cbb246242f4d892f721e4
Jessica Wei <jessicawei9@gmail.com>
2021-03-27 18:50:00 -0700

    + Player Movement + Delegate

--- PlatformOverTheVoidEyesClosed/Assets/Scripts/PlayerMovement.cs
+++ PlatformOverTheVoidEyesClosed/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private Vector3 velocityVector = Vector3.zero;
+    private Vector3 requestedVector = Vector3.zero;
+    [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private float jumpSpeed = 1.0f;
+
+    // Collision checkers & getters
+    private bool isGrounded = true;
+    private bool isCollidingLeft = false;
+    private bool isCollidingRight = false;
+    private bool isCollidingFront = false;
+    private bool isCollidingBack = false;
+    public bool IsGrounded { get { return isGrounded; } }
+    public bool IsCollidingLeft { get { return isCollidingLeft; } }
+    public bool IsCollidingRight { get { return isCollidingRight; } }
+    public bool IsCollidingFront { get { return isCollidingFront; } }
+    public bool IsCollidingBack { get { return isCollidingBack; } }
+
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        UpdateHandler.FixedUpdateOccurred += Movement;
+    }
+
+    private void OnDisable()
+    {
+        UpdateHandler.FixedUpdateOccurred -= Movement;
+    }
+
+    private void Movement() {
+        // Get player input
+        requestedVector = new Vector3(Input.GetAxis("Horizontal")*movementSpeed, rb.velocity.y, Input.GetAxis("Vertical")*movementSpeed);
+        // Move with CharacterController 
+        if(requestedVector != Vector3.zero && movementSpeed != 0)
+            rb.velocity = (requestedVector);
+        // If player wants to Jump
+        if (Input.GetButton("Jump")&&isGrounded)
+        {
+            rb.AddForce(Vector3.up*jumpSpeed);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor")) {
+            isGrounded = true;
+            Debug.Log("Collided with floor.");
+        }
     }
 }
---
+++ PlatformOverTheVoidEyesClosed/Assets/Scripts/UpdateHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateHandler : MonoBehaviour
+{
+    public delegate void onUpdate();
+        public static event onUpdate UpdateOccurred;     // Use this variable to add events/methods onto
+                                                            /* Usage (must have both enable and disable):
+                                                             * -----------------------------------------------
+                                                             * private void OnEnable(){
+                                                             *     UpdateHandler.UpdateOccurred += methodName;
+                                                             * }
+                                                             * private void OnDisable(){
+                                                             *     UpdateHandler.UpdateOccurred -= methodName;
+                                                             * }
+                                                             */
+
+    public delegate void onFixedUpdate();
+    public static event onFixedUpdate FixedUpdateOccurred; // Physics-based updates
+
+    private void Update()
+    {
+        if (UpdateOccurred != null)                   // If there are methods attached to the UpdateOccurred event...
+            UpdateOccurred();                         // Call all of those methods at the same time in one Update() function
+    }
+
+    private void FixedUpdate()
+    {
+        if (FixedUpdateOccurred != null)
+            FixedUpdateOccurred();
+    }
+}
