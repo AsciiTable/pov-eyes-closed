707ddd18d6c6a7eebdbd4b4f25d838f1fdc4de5e
JustinCheung6 <justincheung6@gmail.com>
2021-03-27 23:10:20 -0700

    Audio sources don't sound weird anymore - Remove Doppler Effect in audio properties

---
+++ PlatformOverTheVoidEyesClosed/Assets/Scripts/Sound/WallSound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSound : MonoBehaviour
+{
+    Transform playerTrans = null;
+
+    void Start()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if()
+    }
+}
