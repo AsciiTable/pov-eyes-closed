71582d52edf9ad61e3f248b06829365c171e6af4
JustinCheung6 <justincheung6@gmail.com>
2021-03-29 13:04:53 -0700

    - Messed with friction to no avail - Got Steps working - Got platform started - Updated end goal script to be more precise to where the goal is - Made the basis for the pit object - Made prefabs for endgoal and platform

--- PlatformOverTheVoidEyesClosed/Assets/Scripts/Sound/Pit.cs
+++ PlatformOverTheVoidEyesClosed/Assets/Scripts/Sound/Pit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pit : MonoBehaviour
 {
     Transform playerTrans = null;
+    AudioSource music = null;
 
     //pit is horizontal. Pit is at the verticle if false
     private bool horizontal = false;
@@ -13,37 +14,15 @@ public class Pit : MonoBehaviour
     [Tooltip("How close player needs to be to wall")]
     [SerializeField] private float radius = 5;
 
+    [SerializeField] float volume = 0.3f;
+
     // Start is called before the first frame update
     void Start()
     {
         playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        music = GetComponent<AudioSource>();
 
         horizontal = (transform.localScale.z < transform.localScale.x);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float distance = (horizontal) ? Mathf.Abs(transform.position.z - playerTrans.position.x) : Mathf.Abs(transform.position.z - playerTrans.position.z);
-        distance -= (horizontal) ? (transform.localScale.z + playerTrans.localScale.z) * 0.5f : (transform.localScale.x + playerTrans.localScale.x) * 0.5f;
-
-        if(distance <= radius)
-        {
-            float magnitude = 1 + Mathf.Log(distance / radius);
-            PitSound_Manager.singleton.SetVolume(magnitude);
-            if (!connected)
-            {
-                PitSound_Manager.singleton.ConnectSource();
-                connected = true;
-            }
-        }
-        else
-        {
-            if (connected)
-            {
-                PitSound_Manager.singleton.DisconnectSource();
-                connected = false;
-            }
-        }
+        music.volume = volume;
     }
 }
