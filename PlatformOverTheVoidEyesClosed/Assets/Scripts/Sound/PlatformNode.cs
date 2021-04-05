cc0194b3544d2e1924c2b5759c2d3a97fed51dff
Jessica Wei <jessicawei9@gmail.com>
2021-04-05 01:53:11 -0700

    + Fixed sound progression error by increasing the trigger for platforms of Y by 0.1 in prefab

--- PlatformOverTheVoidEyesClosed/Assets/Scripts/Sound/PlatformNode.cs
+++ PlatformOverTheVoidEyesClosed/Assets/Scripts/Sound/PlatformNode.cs
@@ -6,10 +6,14 @@ public class PlatformNode : ProgressionNode
 {
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("Player")) {
             if (progressionID >= 0)
-                if(col.transform.position.y >= transform.parent.position.y)
+                if (col.transform.position.y >= transform.parent.position.y)
                     SoundProgression_Manager.singleton.Progress(progressionID);
+            Debug.Log("I here ye");
+        }
+
+            
     }
     private void OnTriggerStay(Collider col)
     {
